96c352101a17aef0fa11a48a129e39a7030a802d
xayitboyev_f <1049995@gmail.com>
2021-02-13 00:36:33 +0500

    sticky header

--- js/main.js
+++ js/main.js
@@ -16,3 +16,37 @@ accordionItemHeaders.forEach(accordionItemHeader => {
     });
 });
 
+window.onscroll = function () {
+    scrollFunction()
+}
+
+function scrollFunction() {
+
+    var  intElemScrollTop = document.documentElement.scrollTop;
+    console.log(intElemScrollTop)
+
+
+
+    const checkpoint = 750;
+     const doc = document.getElementById("about")
+    // window.addEventListener("scroll", () => {
+    //     const currentScroll = window.pageYOffset;
+    //     if (currentScroll <= checkpoint) {
+    //         opacity = 1 - currentScroll / checkpoint;
+    //     } else {
+    //         opacity = 0;
+    //     }
+    //     document.querySelector("#about").style.opacity = opacity;
+    // });
+
+    if (document.body.scrollTop > checkpoint || document.documentElement.scrollTop > checkpoint) {
+        doc.classList.add("sticky");
+    } else {
+        doc.classList.remove("sticky");
+    }
+    if (document.body.scrollTop > checkpoint + 2100 || document.documentElement.scrollTop > checkpoint + 2100) {
+        doc.style.opacity = 0;
+    } else {
+        doc.style.opacity = 1;
+    }
+}
